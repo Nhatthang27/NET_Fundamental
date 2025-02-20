81349eeefcc9505b8fed69748d07a361f839ec88
nhatthang270404@gmail.com <nhatthang270404@gmail.com>
2025-02-20 19:54:08 +0700

    Rate Limit

---
+++ RateLimitDemo/Controllers/ApiController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace RateLimitDemo.Controllers
+{
+    [Route("api/v1")]
+    public class ApiController : Controller
+    {
+        private static int _counter = 0;
+
+        [HttpGet("time")]
+        [EnableRateLimiting("ConcurrencyLimiter")]
+        public IActionResult CurrentTime()
+        {
+            _counter++;
+            //Console.WriteLine($"API accessed {_counter} times.");
+
+            var currentTime = DateTime.Now.ToString();
+            return Ok(currentTime);
+        }
+    }
+}
---
+++ RateLimitDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RateLimitDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
